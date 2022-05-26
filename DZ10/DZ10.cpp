ac98c2db5687c401508cd22fcc44424ee814def6
97pinkle <pon.den.03@gmail.com>
2022-05-27 00:16:19 +0300

    Добавьте файлы проекта.

---
+++ DZ10/DZ10.cpp
@@ -0,0 +1,10 @@
+﻿#include "Header.h"
+
+int main()
+{
+    Student ivan("Ivanov", "Ivan", "Ivanovich", 18, true);
+    Boss bos("Boss", "Boss", "Boss", 54, 26);
+    ivan.print();
+    cout << endl;
+    bos.print();
+}
